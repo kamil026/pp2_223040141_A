9f91d8527b6b547301cefb5fbd0c1c7710b3b0b6
kamil026 <kholishkamil54@gmail.com>
2024-12-10 13:54:57 +0700

    push tugas 7 dan 8

--- Pertemuan_6/TabelMahasiswa_JTable.java
+++ Pertemuan_6/TabelMahasiswa_JTable.java
@@ -6,7 +6,7 @@ public class TabelMahasiswa_JTable {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Data Mahasiswa");
 
-        // Data mahasiswa (bisa diganti dengan data dari database atau file)
+        
         String[][] data = {
                 {"Andi", "12345", "Informatika"},
                 {"Budi", "23456", "Sistem Informasi"},
@@ -14,11 +14,11 @@ public class TabelMahasiswa_JTable {
         };
         String[] kolom = {"Nama", "NIM", "Jurusan"};
 
-        // Membuat model tabel
+        
         DefaultTableModel model = new DefaultTableModel(data, kolom);
         JTable table = new JTable(model);
 
-        // Membuat scroll pane agar tabel dapat digulir
+        
         JScrollPane scrollPane = new JScrollPane(table);
 
         frame.add(scrollPane);
